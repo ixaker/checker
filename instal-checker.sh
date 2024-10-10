64d722fb2224c02ac4d5eabee918cf7c9effb438
itSuhsko14 <itsushkoandriy@gmail.com>
2024-10-10 09:14:02 +0300

    add instal-checker.sh

---
+++ instal-checker.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Проверка, запущен ли скрипт под root
+if [ "$(id -u)" -eq 0 ]; then
+    # Скрипт запущен под root
+    SUDO=""
+    echo "Скрипт запущен под root."
+else
+    # Скрипт запущен под другим пользователем
+    SUDO="sudo"
+    echo "Скрипт запущен не под root. Будет использоваться sudo."
+fi
+
+# Функция для проверки наличия пакета
+check_command() {
+    command_name=$1
+    if ! command -v $command_name &> /dev/null; then
+        echo "$command_name не найден! Пожалуйста, установите его."
+        MISSING_PACKAGES+=($command_name)
+    else
+        echo "$command_name установлен."
+    fi
+}
+
+# Проверка наличия необходимых пакетов
+MISSING_PACKAGES=()
+check_command node
+check_command npm
+check_command curl
+check_command git
+
+# Установка недостающих пакетов
+if [ ${#MISSING_PACKAGES[@]} -ne 0 ]; then
+    echo "Некоторые пакеты отсутствуют: ${MISSING_PACKAGES[@]}"
+    echo "Попытка установить отсутствующие пакеты..."
+    
+    $SUDO apt update
+    
+    for package in "${MISSING_PACKAGES[@]}"; do
+        $SUDO apt install -y $package
+    done
+else
+    echo "Все необходимые пакеты установлены."
+fi
+
+echo "Скрипт завершен."
